ede97f926e38c5da2535f779dac694af21a8f6f6
Mohammed iris <150314470+mohammediris@users.noreply.github.com>
2026-04-27 23:09:07 +0400

    v

--- programs/SubnetCalculator/SubnetCalculator/obj/Debug/net8.0-windows/SubnetCalculator.AssemblyInfo.cs
+++ programs/SubnetCalculator/SubnetCalculator/obj/Debug/net8.0-windows/SubnetCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SubnetCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da9d97003d820b3e6300ab87f4b73dfbdc3c19b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a17788fe3d1485e59a54242e7a57e422a68ad93a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SubnetCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SubnetCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
